b41de41e263bc903b6dd8b3812a1a028c1f38f34
MinhLuuu <MinhDQGCH211164@fpt.edu.vn>
2023-06-19 11:55:01 +0700

    Ok

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,24 @@ var router = express.Router();
 router.get('/', async (req, res) => {
   var toys = await ToyModel.find({});
   var total = await ToyModel.count();
-  res.render('index', { toys : toys , total : total })
+  res.render('index', { toys: toys, total: total })
 })
 
 router.get('/customer', async (req, res) => {
   var toys = await ToyModel.find({});
-  res.render('customer', { toys : toys });
+  res.render('customer', { toys: toys });
 })
 
-router.get('/delete/:id', async(req, res) => {
+router.get('/delete/:id', async (req, res) => {
   await ToyModel.findByIdAndDelete(req.params.id)
-  .then(() => { console.log ('Delete Toy succeed !')})
-  .catch((err) => { console.log ('Delete toy failed !')});
+    .then(() => { console.log('Delete Toy succeed !') })
+    .catch((err) => { console.log('Delete toy failed !') });
   res.redirect('/');
 })
 
-router.get('/drop', async(req, res) => {
+router.get('/drop', async (req, res) => {
   await ToyModel.deleteMany({})
-  .then(() => { console.log ('Delete all toys succeed !')});
+    .then(() => { console.log('Delete all toys succeed !') });
   res.redirect('/');
 })
 
@@ -33,24 +33,42 @@ router.get('/add', (req, res) => {
 router.post('/add', async (req, res) => {
   var toys = req.body;
   await ToyModel.create(toys)
-  .then(() => { console.log ('Add new mobile succeed !')});
+    .then(() => { console.log('Add new toy succeed !') });
   res.redirect('/');
 })
 
 router.get('/edit/:id', async (req, res) => {
   var toys = await ToyModel.findById(req.params.id);
-  res.render('edit', { toys : toys});
+  res.render('edit', { toys: toys });
 })
 
 router.post('/edit/:id', async (req, res) => {
   var id = req.params.id;
   await ToyModel.findByIdAndUpdate(id)
-  .then(() => { console.log('Edit toy succeed !') });
+    .then(() => { console.log('Edit toy succeed !') });
   res.redirect('/');
 })
 
+router.get('/order/:id', async (req, res) => {
+  var toys = await ToyModel.findById(req.params.id);
+  res.render('order', { toys: toys })
+})
+
+router.post('/confirm', (req, res) => {
+  var order = req.body;
+  var image = order.image;
+  var name = order.name;
+  var quantity = order.quantity;
+  var price = order.price;
+  var total = price * quantity;
+  console.log("Toy Name: " + name);
+  console.log("Order quantity : " + quantity);
+  console.log("Total price: " + total);
+  res.render('confirm', { name: name, quantity: quantity, price: price, total: total });
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
